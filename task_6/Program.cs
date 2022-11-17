457c659e7c22bc0fcbec566f92e2bc5f92c504d5
Ksenia Ershova <58181386+ODISEIIA@users.noreply.github.com>
2022-11-17 20:14:21 +0300

    HW_C#

---
+++ task_6/Program.cs
@@ -0,0 +1,17 @@
+﻿    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+          
+            if (a % 2 == 0) 
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число нечетное");
+            }
+        }
+    }
