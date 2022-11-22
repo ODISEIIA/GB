6a06e6e46c6bde6aad491d746b45e9700f139b3b
Ksenia Ershova <58181386+ODISEIIA@users.noreply.github.com>
2022-11-22 19:58:22 +0300

    Update tsk21

--- task21/Program.cs
+++ task21/Program.cs
@@ -1,29 +1,29 @@
-﻿// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int x1 = Coordinate("x", "A");
-int y1 = Coordinate("y", "A");
-int z1 = Coordinate("z", "A");
-int x2 = Coordinate("x", "B");
-int y2 = Coordinate("y", "B");
-int z2 = Coordinate("z", "B");
+Console.Write("Введите точку XA: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку YA: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку ZA: ");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку XB: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку YB: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите точку ZB: ");
+int zb = Convert.ToInt32(Console.ReadLine());
 
-int Coordinate(string coorName, string pointName)
+double Distance(int xa, int ya, int za, int xb, int yb, int zb)
 {
-    Console.Write($"Введите координату {coorName} точки {pointName}: ");
-    return Convert.ToInt16(Console.ReadLine());
+    double xc = (xa - xb) * (xa - xb);
+    double yc = (ya - yb) * (ya - yb);
+    double zc = (za - zb) * (za - zb);
+    double result = Math.Sqrt(xc + yc + zc);
+    return result;
 }
 
-double Decision(double x1, double x2,
-                double y1, double y2,
-                double z1, double z2)
-{
-    return Math.Sqrt(Math.Pow((x2 - x1), 2) +
-                    Math.Pow((y2 - y1), 2) +
-                    Math.Pow((z2 - z1), 2));
-}
-
-double segmentLength = Math.Round(Decision(x1, x2, y1, y2, z1, z2), 2);
-
-Console.WriteLine($"Длина отрезка {segmentLength}");
+double res = Distance(xa, ya, za, xb, yb, zb);
+double resRound = Math.Round(res, 2, MidpointRounding.ToZero);
+Console.WriteLine(resRound);
