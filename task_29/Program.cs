7f69392c01992b55912b6b15019482c11ecfbc8d
Ksenia Ershova <58181386+ODISEIIA@users.noreply.github.com>
2022-11-29 02:03:32 +0300

    HW_C#_4

---
+++ task_29/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+internal partial class Program
+{
+    private static void Main(string[] args)
+    {
+        {
+            int[] array = new int[8];
+            Random myRandom = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = myRandom.Next(0, 7);
+                Console.Write("{0} ", array[i]);
+            }
+        }
+    }
+}
