00d5e25ae95abfdff6f1c282fa4aa0ced7fbda1b
Ksenia Ershova <58181386+ODISEIIA@users.noreply.github.com>
2022-11-29 01:40:17 +0300

    HW_C#_4

---
+++ task_25/Program.cs
@@ -0,0 +1,16 @@
+﻿    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите необходимую степень числа:");
+            int b = Convert.ToInt32(Console.ReadLine());
+          
+            int x = 1; 
+            for (int i = 0; i<b; i++ ) {
+                x*=a;
+            }
+            Console.WriteLine("{0} ^ {1} = {2}", a, b, x);
+    }
+    }
