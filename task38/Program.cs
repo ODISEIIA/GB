4c5acfe31cfe00a34e184118f861c9a0ff1cbf8a
Ksenia Ershova <58181386+ODISEIIA@users.noreply.github.com>
2022-12-05 14:57:58 +0300

    HW_C#_5

---
+++ task38/Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементами массива.
+//[3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+double [] CreateArr(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+    return arr;
+}
+
+double [] arr = CreateArr (5, -10, 10);
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+}
+
+void PrintArr(double [] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+          }
+}
+
+Console.Write("[ ");
+PrintArr(arr);
+Console.WriteLine($" ] -> {max - min}");
+
+
